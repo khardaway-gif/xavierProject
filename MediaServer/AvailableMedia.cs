0522323112540d14080a1c51ab97fc33c301e2f7
khardaway-gif <158842208+khardaway-gif@users.noreply.github.com>
2025-04-29 14:40:23 -0400

    Update AvailableMedia.cs

--- MediaServer/AvailableMedia.cs
+++ MediaServer/AvailableMedia.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.IO;
 
 namespace MediaServer
@@ -14,8 +14,12 @@ namespace MediaServer
             if (AvailableMedia.path == null || !AvailableMedia.path.Equals(path))
             {
                 AvailableMedia.path = path;
-                fileArray = Directory.GetFiles(path, "*.mp3", SearchOption.AllDirectories).Union(Directory.GetFiles(path, "*.mp4", SearchOption.AllDirectories)).ToArray();
-                //TODO: ADD jpg, png, and gif to the fileArray 
+                fileArray = Directory.GetFiles(path, "*.mp3", SearchOption.AllDirectories)
+                    .Union(Directory.GetFiles(path, "*.mp4", SearchOption.AllDirectories))
+                    .Union(Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories)) // Added jpg
+                    .Union(Directory.GetFiles(path, "*.png", SearchOption.AllDirectories)) // Added png
+                    .Union(Directory.GetFiles(path, "*.gif", SearchOption.AllDirectories)) // Added gif
+                    .ToArray();
             }
         }
         public IEnumerable<string> getAvailableFiles()
